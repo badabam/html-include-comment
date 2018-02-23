5658249c73c948e9ecbe4ad7f9339bec1d393f8c
Jeremias Erbs <jerry@badabam.de>
2018-02-23 15:45:21 +0100

    Finish first version

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # html-include-comment
 Simply include html partials via html comments like this &lt;!-- header.html -->
+
+# Install
+`$ npm install html-include-comment`
+
+# Usage
+`$ html-include-comment some/source.html some/dest.html`
+
+# Patterns
+
+It works with both comment styles: `<!-- some/file.html -->` and `/* some/otherfile.css */`.
+
+File type or extension does not matter.
---
+++ index.js
@@ -0,0 +1,68 @@
+#! /usr/bin/env node
+
+const fs = require('fs')
+const Q = require('q')
+const path = require('path')
+const pkg = require('./package.json')
+
+const fs_readFile = Q.denodeify(fs.readFile)
+const fs_writeFile = Q.denodeify(fs.writeFile)
+
+const patterns = [/<!-- .* -->/g, /\/\* .* \*\//g]
+
+const program = require('commander')
+
+program
+  .version(pkg.version, '-v, --version')
+  .arguments('<srcFile> <targetFile>')
+  .option('-s', '--silent', 'No output')
+  .parse(process.argv)
+
+const [srcFile, targetFile] = program.args
+
+renderPage(srcFile, targetFile)
+
+function renderPage(srcFile, targetFile) {
+  const deferred = Q.defer()
+  fs_readFile(srcFile, 'utf-8').done(data => {
+    getMatches(data, deferred, targetFile)
+  })
+
+  return deferred.promise
+}
+
+function getMatches(mainFileData, deferred, targetFile) {
+  if (!mainFileData.length) {
+    deferred.reject('Empty file.')
+  }
+
+  const matches = patterns.reduce((prev, curr) => {
+    const currentMatch = mainFileData.match(curr)
+    if (currentMatch.length) {
+      return prev.concat(currentMatch)
+    }
+    return prev
+  }, [])
+
+  let promises = []
+
+  // beware: you can't use map to return promises
+  matches.forEach(match => {
+    const partialPath = match.split(' ')[1]
+    const srcPath = path.dirname(path.join(process.cwd(), srcFile))
+
+    const p = fs_readFile(path.join(srcPath, partialPath), 'utf8')
+
+    p.done(partialData => {
+      mainFileData = mainFileData.replace(match, partialData)
+    })
+    promises.push(p)
+  })
+
+  return Q.all(promises).done(() => {
+    fs_writeFile(targetFile, mainFileData).done(() => {
+      program.silent || console.log(`Rendered ${targetFile}`)
+      deferred.resolve()
+    })
+  })
+}
